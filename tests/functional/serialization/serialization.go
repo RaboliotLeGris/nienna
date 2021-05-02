9d714ba7bb22e3af2b91e24e7e8f4942c5207553
RaboliotLeGris <jordan@raboland.fr>
2021-05-02 21:39:24 +0200

    tests: add several others tests

---
+++ tests/functional/serialization/serialization.go
@@ -0,0 +1,26 @@
+package serialization
+
+type UserLogin struct {
+	Username string `json:"username"`
+}
+
+type UserRegister struct {
+	Username string `json:"username"`
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+type Video struct {
+	Slug        string `json:"slug"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	Uploader    User   `json:"uploader"`
+}
+
+type StatusSerialized struct {
+	Status string `json:"status"`
+}

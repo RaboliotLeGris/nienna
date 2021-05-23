a452b4831b14bf5a282553cdc0320cad2a849094
RaboliotLeGris <jordan@raboland.fr>
2021-05-23 20:11:14 +0200

    backburner: amqp reconnection

--- backburner/src/clients/amqp/client.rs
+++ backburner/src/clients/amqp/client.rs
@@ -3,9 +3,11 @@ use lapin::{Channel, Connection, ConnectionProperties, Consumer, options::*, typ
 
 use crate::clients::amqp::errors::AmqpError;
 use crate::clients::amqp::serialization::EventSerialization;
+use std::time::Duration;
 
 pub struct AMQP {
-    _conn: Connection,
+    uri: String,
+    conn: Connection,
     channel: Channel,
     consumer: Option<Consumer>,
     queue: String,
@@ -13,40 +15,10 @@ pub struct AMQP {
 
 impl AMQP {
     pub async fn new(uri: String, queue: String, enable_consumer: bool) -> Self {
-        let conn = Connection::connect(uri.as_str(), ConnectionProperties::default())
-            .await
-            .expect("connection error");
-        info!("Connected to rabbitMQ");
-
-        let channel = conn.create_channel().await.expect("create_channel");
-        debug!("Channel status {:?}", conn.status().state());
-
-        debug!("Declaring queue: \"{}\"", queue);
-        let _queue = channel
-            .queue_declare(
-                queue.as_str(),
-                QueueDeclareOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .expect("queue_declare");
-
-        let mut consumer = None;
-        if enable_consumer {
-            debug!("Create consumer for queue: \"{}\"", queue);
-            consumer = Some(channel
-                .basic_consume(
-                    queue.as_str(),
-                    "backburner",
-                    BasicConsumeOptions::default(),
-                    FieldTable::default(),
-                )
-                .await
-                .expect("basic_consume"));
-        }
-
+        let (conn, channel, consumer) = AMQP::connect(&uri, &queue, enable_consumer).await.expect("Must establish connection with AMQP server");
         AMQP {
-            _conn: conn,
+            uri,
+            conn,
             channel,
             consumer,
             queue,
@@ -57,24 +29,92 @@ impl AMQP {
         if self.consumer.is_none() {
             return Err(AmqpError::ConsumerIsNone);
         }
+        if self.conn.status().state() != lapin::ConnectionState::Connected {
+            self.reconnect().await?;
+        }
         if let Some(Ok(delivery)) = self.consumer.as_mut().unwrap().next().await {
-                let _ = delivery.0.basic_ack(delivery.1.delivery_tag, BasicAckOptions::default()).await;
-                return EventSerialization::from(delivery.1.data);
+            let _ = delivery.0.basic_ack(delivery.1.delivery_tag, BasicAckOptions::default()).await;
+            return EventSerialization::from(delivery.1.data);
         }
         Err(AmqpError::FailFetchEvent)
     }
 
     pub async fn publish(&mut self, payload: Vec<u8>) -> Result<(), AmqpError> {
+        if self.conn.status().state() != lapin::ConnectionState::Connected {
+            self.reconnect().await?;
+        }
         if self.channel.basic_publish("", self.queue.as_str(), BasicPublishOptions::default(), payload, BasicProperties::default()).await.is_ok() {
             return Ok(());
         }
         Err(AmqpError::FailPublishEvent)
     }
 
-    pub async fn ack(&mut self, delivery_tag: u64) -> Result<(), AmqpError> {
-        if self.channel.basic_ack(delivery_tag, BasicAckOptions::default()).await.is_ok() {
-            return Ok(());
+    async fn connect(uri: &String, queue: &String, enable_consumer: bool) -> Result<(Connection, Channel, Option<Consumer>), AmqpError> {
+        let (conn, channel) = AMQP::create_conn(uri).await?;
+        let _ = AMQP::create_queue(&channel, &queue).await?;
+
+        let mut consumer = None;
+        if enable_consumer {
+            debug!("Create consumer for queue: \"{}\"", queue);
+            consumer = Some(AMQP::create_consumer(&channel, &queue).await?);
         }
-        Err(AmqpError::FailPublishEvent)
+
+        Ok((conn, channel, consumer))
+    }
+
+    async fn reconnect(&mut self) -> Result<(), AmqpError> {
+        let mut retry_count = 10;
+        let mut connected = false;
+        while !connected && retry_count > 0 {
+            if let Ok((conn, channel, consumer)) = AMQP::connect(&self.uri, &self.queue, self.consumer.is_some()).await {
+                self.conn = conn;
+                self.channel = channel;
+                self.consumer = consumer;
+                connected = true;
+                break;
+            }
+            retry_count = retry_count - 1;
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
+        if !connected {
+            return Err(AmqpError::ConnectionFailed);
+        }
+        Ok(())
+    }
+
+    async fn create_conn(uri: &String) -> Result<(Connection, Channel), AmqpError> {
+        info!("Connecting to rabbitMQ");
+        let conn = Connection::connect(uri.as_str(), ConnectionProperties::default())
+            .await?;
+
+        info!("Creating amqp channel");
+        let channel = conn.create_channel().await?;
+        debug!("Channel status {:?}", conn.status().state());
+
+        Ok((conn, channel))
+    }
+
+    async fn create_queue(channel: &Channel, queue: &String) -> Result<(), AmqpError> {
+        debug!("Declaring queue: \"{}\"", queue);
+        let _queue = channel
+            .queue_declare(
+                queue.as_str(),
+                QueueDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn create_consumer(channel: &Channel, queue: &String) -> Result<Consumer, AmqpError> {
+        let consumer = channel
+            .basic_consume(
+                queue.as_str(),
+                "backburner",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+        Ok(consumer)
     }
 }
--- backburner/src/clients/amqp/errors.rs
+++ backburner/src/clients/amqp/errors.rs
@@ -1,11 +1,15 @@
+use lapin::Error;
+
 #[derive(Debug)]
-pub enum AmqpError{
+pub enum AmqpError {
     FailFetchEvent,
     FailDeserialization,
     FailSerialization,
     FailPublishEvent,
     ConsumerIsNone,
     UnrecognizedEvent,
+    ConnectionFailed,
+    QueueCreationFailed
 }
 
 impl std::error::Error for AmqpError {}
@@ -18,7 +22,26 @@ impl std::fmt::Display for AmqpError {
             AmqpError::FailFetchEvent => write!(f, "Failed to fetch an event"),
             AmqpError::FailPublishEvent => write!(f, "Failed publish an event"),
             AmqpError::UnrecognizedEvent => write!(f, "Unable to recognize event"),
-            AmqpError::ConsumerIsNone => write!(f, "No consumer created")
+            AmqpError::ConsumerIsNone => write!(f, "No consumer created"),
+            AmqpError::ConnectionFailed => write!(f, "Connection failed"),
+            AmqpError::QueueCreationFailed => write!(f, "Failed to create a queue"),
+        }
+    }
+}
+
+impl From<lapin::Error> for AmqpError {
+    fn from(e: lapin::Error) -> Self {
+        match e {
+            Error::ChannelsLimitReached => AmqpError::ConnectionFailed,
+            Error::InvalidProtocolVersion(_) =>  AmqpError::UnrecognizedEvent,
+            Error::InvalidChannel(_) => AmqpError::ConnectionFailed,
+            Error::InvalidChannelState(_) =>  AmqpError::UnrecognizedEvent,
+            Error::InvalidConnectionState(_) => AmqpError::ConnectionFailed,
+            Error::IOError(_) =>  AmqpError::UnrecognizedEvent,
+            Error::ParsingError(_) =>  AmqpError::UnrecognizedEvent,
+            Error::ProtocolError(_) =>  AmqpError::UnrecognizedEvent,
+            Error::SerialisationError(_) => AmqpError::FailSerialization,
+            _ => AmqpError::UnrecognizedEvent
         }
     }
 }

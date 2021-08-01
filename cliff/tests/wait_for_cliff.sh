9dc19027e5c9501b4e655623942265ee7425f3de
RaboliotLeGris <jordan@raboland.fr>
2021-08-01 15:51:57 +0200

    docker: fix tests

--- cliff/tests/wait_for_cliff.sh
+++ cliff/tests/wait_for_cliff.sh
@@ -4,14 +4,18 @@ CLIFF_HOST="${CLIFF_HOST:-"http://localhost"}"
 CLIFF_ROUTE="/api/health"
 TRIES=0
 
-while [ `curl -s -o /dev/null -w "%{http_code}\n" $CLIFF_HOST$CLIFF_ROUTE` -ne 200 ] && [ $TRIES -ne 8 ]
+echo "$CLIFF_HOST$CLIFF_ROUTE"
+while [ `curl -v -s -o /dev/null -w "%{http_code}\n" $CLIFF_HOST$CLIFF_ROUTE` -ne 200 ] && [ $TRIES -ne 10 ]
 do
+  docker logs -f "name=nienna_redis"
+  echo "`curl -v -s -o /dev/null -w "%{http_code}\n" $CLIFF_HOST$CLIFF_ROUTE`"
   TRIES=$((TRIES+1))
   echo "Waiting for Cliff to start up. Tries: $TRIES/8"
   sleep 5
 done
+  echo "`curl -v -s -o /dev/null -w "%{http_code}\n" $CLIFF_HOST$CLIFF_ROUTE`"
 
-if [ $TRIES -ge 8 ]
+if [ $TRIES -ge 10 ]
 then
   echo "Failed to access cliff"
   exit 1
